ad16c120ba322c4859945db012ec12db8a1e3387
AgnieszkaKaliszewska <nesi1@interia.pl>
2018-09-29 14:50:09 +0200

    zadanie 2 -zmiana rozwijanych funkcji więcej/mniej

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
 	
 	var submenu= document.querySelector ( '.dropdown' );
 	
+	
 	menuButton.addEventListener( 'mouseover' ,  function(){
 	submenu.style.display= 'block' ;
 	
@@ -15,6 +16,25 @@ menuButton.addEventListener( 'mouseout' ,  function(){
 	submenu.style.display= 'none' ;
 	
     });
+	
+var buttons = document.querySelectorAll( '.read-more' );
+// console.log(buttons);
+	
+	function showHide(){
+		var textArea = this.previousElementSibling; //  pobiera poprzedniego sąsiada z góry
+		      if (textArea.style.display === 'none' || textArea.style.display === '') {
+         console.log(textArea)
+	     textArea.style.display = 'block'; 
+			  this.innerHTML= 'MNIEJ <span class="gryphicon gryphicon-chevron-up"></span>' ;
+			  }
+			  else {
 
+        textArea.style.display = 'none';
+        this.innerHTML = 'WIĘCEJ <span class="glyphicon glyphicon-chevron-down"></span>';
+	}
+	}
+	  for (var i = 0; i <buttons.length; i++) {
+        buttons[i].addEventListener('click', showHide);
+    }
 	
 });
